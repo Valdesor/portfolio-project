ae9aac13df22225f1602bfe52112ffb7e6ede98d
igor <igor.bernatskyi@gmail.com>
2018-05-12 13:13:15 +0000

    preloader

---
+++ js/preloader.js
@@ -0,0 +1,13 @@
+// JavaScript File
+var preload = document.createElement('div');
+
+preload.className = "preloader";
+preload.innerHTML = '<div class="b-ico-preloader"></div><div class="spinner"></div>';
+document.body.appendChild(preload);
+
+window.addEventListener('load', function() {
+  preload.className +=  ' fade';
+  setTimeout(function(){
+  preload.style.display = 'none';
+  },600);
+})
